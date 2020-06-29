7ffe3de68672f6092e7b355cf66082c287a4c97b
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-29 10:12:06 -0300

    Add project files.

---
+++ Conversor_Dolar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conversor_Dolar
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Hello World!");
+            }
+        }
+    }
